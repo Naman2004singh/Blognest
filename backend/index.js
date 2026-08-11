25747e5a48e7bf2114d9558c60d458a1529fd349
Naman2004singh <naman.2singh9@gmail.com>
2026-08-11 16:17:17 +0530

    add database connection and error handling in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,24 @@
 import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./src/db/index.js";
 
 dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+    .then( () => {
+
+        // after the sucessfull connection, we have register this event listner
+        app.on("error", (error) => {
+            console.log("ERR : ", error);
+            process.exit(1);  
+        })
+
+        app.listen(PORT, () => {
+            console.log(`Server is ruunig on port ${PORT}` );     
+        });
+    })
+    .catch((error) => {
+        console.error("Database connection failed:", error);
+    })
